94c59614ddafccca25c472f47678081816cff1a4
Mehul Patel <mehul8@gmail.com>
2014-01-18 16:31:29 -0800

    adds more tests

--- test/api_spec.js
+++ test/api_spec.js
@@ -11,4 +11,22 @@ frisby.create('GET hello world').get(URL).expectStatus(200).expectJSONTypes({
   test: 'hello world'
 }).toss();
 
-frisby.create('POST new user').post("" + URL + "/signup").expectStatus(500).toss();
+frisby.create('GET /test').get("" + URL + "/test").expectStatus(500).toss();
+
+frisby.create('POST new user without apikey').post("" + URL + "/signup", {
+  username: 'test',
+  password: 'test'
+}).expectStatus(500).toss();
+
+frisby.globalSetup({
+  request: {
+    headers: {
+      apikey: "myKey"
+    }
+  }
+});
+
+frisby.create('POST /signup with apikey').post("" + URL + "/signup", {
+  username: 'test',
+  password: 'test'
+}).expectStatus(201).expectHeaderContains("Content-Type", "text/plain").toss();
